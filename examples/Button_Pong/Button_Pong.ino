6d89afd558dd857d90d45222cff4daedc10b8d87
Thedappolice <155630521+Thedappolice@users.noreply.github.com>
2024-01-15 22:16:14 +0800

    update

--- examples/Button_Pong/Button_Pong.ino
+++ examples/Button_Pong/Button_Pong.ino
@@ -1,9 +1,9 @@
 #include <LEDMatrix.h>
 
-int posPin[] = {6, 7, 8, 9, 10, 11, 12, 13};
-int negPin[] = {A0, A1, A2, A3, A4, A5, A6, A7};
+int posPin[] = { 6, 7, 8, 9, 10, 11, 12, 13 };
+int negPin[] = { A0, A1, A2, A3, A4, A5, A6, A7 };
 
-int wall[] = {0, 0, 0, 1, 1, 1, 0, 0};
+int wall[] = { 0, 0, 0, 1, 1, 1, 0, 0 };
 
 LEDMatrix LM(posPin, 8, negPin, 8);
 
@@ -12,10 +12,12 @@ LEDMatrix LM(posPin, 8, negPin, 8);
 #define P2left 2
 #define P2right 3
 
+int timeupdate;
+
 int ballX;
 int ballY;
-int Xdirection = random(0, 1);
-int Ydirection = random(0, 1);
+int Xdirection = random(2);
+int Ydirection = random(2);
 
 int P1leftstat;
 int P1rightstat;
@@ -25,64 +27,166 @@ int P2rightstat;
 int P1shift = 0;
 int P2shift = 0;
 
-int memory[8][8] =
-    {
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {1, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 1},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0}};
-
-void setup()
-{
-    for (int i = 2; i < 6; i++)
-    {
-        pinMode(i, INPUT);
-    }
-}
-
-void loop()
-{
-    display();
-    checkbutton();
-    checklogic(memory);
-}
-
-void checkbutton()
-{
-    P1leftstat = digitalRead(P1left);
-    P1rightstat = digitalRead(P1right);
-    P2leftstat = digitalRead(P2left);
-    P2rightstat = digitalRead(P2right);
+int memory[8][8] = {
+  { 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 1, 0, 0, 0, 0, 0, 0, 1 },
+  { 1, 0, 0, 0, 0, 0, 0, 1 },
+  { 1, 0, 0, 0, 0, 0, 0, 1 },
+  { 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0 }
 };
 
-void display()
-{
-    LM.customCol(wall, 0, P1shift);
-    LM.customCol(wall, 8, P2shift);
-    LM.turnOn(ballX, ballY);
+int W[8][8] = {
+  { 1, 0, 0, 0, 0, 0, 0, 1 },
+  { 1, 0, 0, 0, 0, 0, 0, 1 },
+  { 1, 0, 0, 1, 1, 0, 0, 1 },
+  { 1, 1, 0, 1, 1, 0, 1, 1 },
+  { 1, 1, 0, 1, 1, 0, 1, 1 },
+  { 1, 1, 1, 0, 0, 1, 1, 1 },
+  { 0, 1, 1, 0, 0, 1, 1, 0 },
+  { 0, 1, 0, 0, 0, 0, 1, 0 }
 };
 
-void checklogic(int memory[][8])
-{
-    if (ballX == 1 || ballX == 6)
-    {
-    }
+int I[8][8] = {
+  { 0, 1, 1, 1, 1, 1, 1, 0 },
+  { 0, 1, 1, 1, 1, 1, 1, 0 },
+  { 0, 0, 0, 1, 1, 0, 0, 0 },
+  { 0, 0, 0, 1, 1, 0, 0, 0 },
+  { 0, 0, 0, 1, 1, 0, 0, 0 },
+  { 0, 0, 0, 1, 1, 0, 0, 0 },
+  { 0, 1, 1, 1, 1, 1, 1, 0 },
+  { 0, 1, 1, 1, 1, 1, 1, 0 }
 };
 
-int limitingshift(int value, bool change)
+int N[8][8] = {
+  { 1, 1, 0, 0, 0, 0, 1, 1 },
+  { 1, 1, 1, 0, 0, 0, 1, 1 },
+  { 1, 1, 1, 1, 0, 0, 1, 1 },
+  { 1, 1, 1, 1, 1, 0, 1, 1 },
+  { 1, 1, 0, 1, 1, 1, 1, 1 },
+  { 1, 1, 0, 0, 1, 1, 1, 1 },
+  { 1, 1, 0, 0, 0, 1, 1, 1 },
+  { 1, 1, 0, 0, 0, 0, 1, 1 }
+};
+
+int P[8][8] = {
+  { 0, 1, 1, 1, 1, 1, 0, 0 },
+  { 0, 1, 1, 0, 0, 0, 1, 0 },
+  { 0, 1, 1, 0, 0, 0, 1, 0 },
+  { 0, 1, 1, 1, 1, 1, 0, 0 },
+  { 0, 1, 1, 0, 0, 0, 0, 0 },
+  { 0, 1, 1, 0, 0, 0, 0, 0 },
+  { 0, 1, 1, 0, 0, 0, 0, 0 },
+  { 0, 1, 1, 0, 0, 0, 0, 0 }
+};
+
+int P1[8][8] = {
+  { 0, 0, 0, 1, 1, 0, 0, 0 },
+  { 0, 0, 1, 1, 1, 0, 0, 0 },
+  { 0, 1, 1, 1, 1, 0, 0, 0 },
+  { 0, 0, 0, 1, 1, 0, 0, 0 },
+  { 0, 0, 0, 1, 1, 0, 0, 0 },
+  { 0, 0, 0, 1, 1, 0, 0, 0 },
+  { 0, 1, 1, 1, 1, 1, 1, 0 },
+  { 0, 1, 1, 1, 1, 1, 1, 0 }
+};
+
+int P2[8][8] = {
+  { 0, 0, 1, 1, 1, 1, 0, 0 },
+  { 0, 1, 1, 1, 1, 1, 1, 0 },
+  { 0, 1, 1, 0, 0, 1, 1, 0 },
+  { 0, 0, 0, 0, 1, 1, 1, 0 },
+  { 0, 0, 0, 1, 1, 1, 0, 0 },
+  { 0, 0, 1, 1, 1, 0, 0, 0 },
+  { 0, 1, 1, 1, 1, 1, 1, 0 },
+  { 0, 1, 1, 1, 1, 1, 1, 0 }
+};
+
+void displaywithtime(int Matrix[][8], int time = 1000) {
+  timeupdate = millis();
+  while (millis() - timeupdate < time) {
+    LM.Symbol(Matrix);
+  }
+};
+
+void checkbutton() {
+  P1leftstat = digitalRead(P1left);
+  P1rightstat = digitalRead(P1right);
+  P2leftstat = digitalRead(P2left);
+  P2rightstat = digitalRead(P2right);
+};
+
+void display() {
+  LM.Symbol(memory);
+  LM.turnOn(ballX, ballY);
+};
+
+void checklogic(int memory[][8]){
+
+};
+
+int limitingshift(int value, bool change) {
+  int mem;
+  if (value < 3 || change == true) {
+    mem = value++;
+  } else if (value > -4 || change == false) {
+    mem = value--;
+  }
+  return mem;
+};
+
+int updateMem(int Matrix[][8])
 {
-    int mem;
-    if (value < 3 || change == true)
-    {
-        mem = value++;
+  int newMatrix[8][8];
+ for(int i = 0; i<8; i++)
+ {
+  for(int j = 0; j<8;j++)
+  {
+    newMatrix[j][i] = ;
+  }
+ }
+ return newMatrix;
+};
+
+void End(int result, bool simple = false) {
+  if (!simple) {
+    for (int i = 0; i < 3; i++) {
+      displaywithtime(P);
+      if (result == 1) {
+        displaywithtime(P1);
+      } else if (result == 2) {
+        displaywithtime(P2);
+      }
+      displaywithtime(W);
+      displaywithtime(I);
+      displaywithtime(N);
     }
-    else if (value > -4 || change == false)
-    {
-        mem = value--;
+
+  } else {
+    for (int i = 0; i < 30; i++) {
+      if (result == 1) {
+        displaywithtime(P1, 300);
+      } else if (result == 2) {
+        displaywithtime(P2, 300);
+      }
+      delay(300);
     }
-    return mem;
-}
+  }
+  exit(1);
+};
+
+void setup() {
+  for (int i = 2; i < 6; i++) {
+    pinMode(i, INPUT);
+  }
+};
+
+void loop() {
+  displaywithtime(memory);
+  // memory = updateMem(memory);
+  // display();
+  // checkbutton();
+  // checklogic();
+};
