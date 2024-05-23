45d0a639b6a2e672073194552cddeecc07a7eb59
Thedappolice <155630521+Thedappolice@users.noreply.github.com>
2024-05-23 20:49:10 +0800

    it workkssss

--- examples/Joystick_snake/Joystick_snake.ino
+++ examples/Joystick_snake/Joystick_snake.ino
@@ -6,16 +6,46 @@ int negPin[] = {10, 11, 12, 13, A0, A1, A2, A3};
 
 LEDMatrix LM(posPin, 8, negPin, 8);
 
-// Joystick inputs
-int JoyX;
-int JoyY;
+unsigned long currentMillis;
+unsigned long previousMillis = 0;
+unsigned long interval = 500; // Refresh interval in milliseconds
 
 int length = 3;
-int body[64][2] = {{0}}; // Assuming a maximum length of 64 for the body
+int body[64][2] = {
+    {4, 4},
+    {5, 4},
+    {6, 4},
+};
 int head[2] = {4, 4};
 
 bool foodExists = false;
-int Mayfood[64][2] = {{0}}; // Assuming a maximum of 64 possible food locations
+int Mayfood[64][2] = {{0}};
+// Assuming a maximum of 64 possible food locations
+
+// End and number matrices
+int End[8][8] = {#include <LEDMatrix.h>
+
+// Pin configurations
+int posPin[] = {2, 3, 4, 5, 6, 7, 8, 9};
+int negPin[] = {10, 11, 12, 13, A0, A1, A2, A3};
+
+LEDMatrix LM(posPin, 8, negPin, 8);
+
+unsigned long currentMillis;
+unsigned long previousMillis = 0;
+unsigned long interval = 500; // Refresh interval in milliseconds
+
+int length = 3;
+int body[64][2] = {
+    {4, 4},
+    {5, 4},
+    {6, 4},
+};
+int head[2] = {4, 4};
+
+bool foodExists = false;
+int Mayfood[64][2] = {{0}};
+// Assuming a maximum of 64 possible food locations
 
 // End and number matrices
 int End[8][8] = {
@@ -28,7 +58,7 @@ int End[8][8] = {
     {1, 1, 1, 1, 0, 1, 0, 1},
     {0, 0, 0, 0, 0, 0, 0, 0}};
 
-int Win1[8][8] = {
+int Win[8][8] = {
     {0, 0, 0, 1, 0, 0, 0, 0},
     {0, 0, 0, 1, 0, 0, 0, 0},
     {0, 0, 1, 1, 1, 0, 0, 0},
@@ -37,15 +67,6 @@ int Win1[8][8] = {
     {0, 0, 1, 1, 1, 0, 0, 0},
     {0, 1, 1, 0, 1, 1, 0, 0},
     {1, 1, 0, 0, 0, 1, 1, 0}};
-int Win2[8][8] = {
-    {0, 0, 0, 0, 1, 0, 0, 0},
-    {0, 0, 0, 0, 1, 0, 0, 0},
-    {0, 0, 0, 1, 1, 1, 0, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1},
-    {0, 0, 1, 1, 1, 1, 1, 0},
-    {0, 0, 0, 1, 1, 1, 0, 0},
-    {0, 0, 1, 1, 0, 1, 1, 0},
-    {0, 1, 1, 0, 0, 0, 1, 1}};
 
 int N1[8][8] = {
     {0, 0, 0, 1, 1, 0, 0, 0},
@@ -93,14 +114,14 @@ char direction = 'u';
 bool end = false;
 bool win = false;
 
-void displaywithtime(int Matrix[][8], int time = 1000)
+void displaywithtime(int Matrix[][8], unsigned long time = 1000)
 {
     unsigned long timeupdate = millis();
     while (millis() - timeupdate < time)
     {
         LM.Symbol(Matrix);
     }
-};
+}
 
 void checkdirection()
 {
@@ -127,7 +148,8 @@ void checkdirection()
             JYSTCK = 'u';
         }
 
-        char oppdirection;
+        char oppdirection = 'u'; // Initialize oppdirection with a default value
+
         switch (direction)
         {
         case 'u':
@@ -141,6 +163,9 @@ void checkdirection()
             break;
         case 'r':
             oppdirection = 'l';
+            break;
+        default:
+            oppdirection = 'u'; // Default case to ensure oppdirection is always initialized
             break;
         }
 
@@ -196,7 +221,6 @@ void generateFood()
 void refreshMem()
 {
     int prelength = length;
-    bool eat = false;
     // Update head position based on direction
     switch (direction)
     {
@@ -259,7 +283,6 @@ void refreshMem()
     // Check if the snake eats the food
     if (memory[head[0]][head[1]][2] == -1)
     {
-        eat = true;
         prelength++;
         foodExists = false;
     }
@@ -343,7 +366,6 @@ void ending()
         for (int i = 0; i < 5; i++)
         {
             displaywithtime(Win1, 500);
-            displaywithtime(Win2, 500);
         }
     }
     exit(0);
@@ -356,27 +378,19 @@ void setup()
     displaywithtime(N1);
 
     randomSeed(analogRead(A4));
-    body[0][0] = 4;
-    body[0][1] = 4;
-    body[1][0] = 5;
-    body[1][1] = 4;
-    body[2][0] = 6;
-    body[2][1] = 4;
 
     MemtoDisplay();
 
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 10; i++)
     {
         LM.Symbol(display);
-        delay(100);
+        delay(200);
     }
 }
 
 void loop()
 {
-    unsigned long currentMillis = millis();
-    static unsigned long previousMillis = 0;
-    unsigned long interval = 500; // Refresh interval in milliseconds
+    currentMillis = millis();
 
     if (currentMillis - previousMillis >= interval)
     {
@@ -384,6 +398,375 @@ void loop()
         if (!end)
         {
             generateFood();
+            checkdirection();
+            refreshMem();
+            MemtoDisplay();
+            displaywithtime(display, interval);
+        }
+        else
+        {
+            ending();
+        }
+    }
+}
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 1, 0, 1, 1, 0},
+    {1, 1, 1, 1, 0, 1, 0, 1},
+    {0, 0, 0, 0, 0, 0, 0, 0}};
+
+int Win1[8][8] = {
+    {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 0},
+    {0, 1, 1, 1, 1, 1, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0},
+    {0, 1, 1, 0, 1, 1, 0, 0},
+    {1, 1, 0, 0, 0, 1, 1, 0}};
+int Win2[8][8] = {
+    {0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 1, 0, 0},
+    {0, 1, 1, 1, 1, 1, 1, 1},
+    {0, 0, 1, 1, 1, 1, 1, 0},
+    {0, 0, 0, 1, 1, 1, 0, 0},
+    {0, 0, 1, 1, 0, 1, 1, 0},
+    {0, 1, 1, 0, 0, 0, 1, 1}};
+
+int N1[8][8] = {
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0},
+    {0, 1, 1, 1, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 1, 1, 1, 1, 1, 1, 0},
+    {0, 1, 1, 1, 1, 1, 1, 0}};
+
+int N2[8][8] = {
+    {0, 0, 1, 1, 1, 1, 0, 0},
+    {0, 1, 1, 1, 1, 1, 1, 0},
+    {0, 1, 1, 0, 0, 1, 1, 0},
+    {0, 0, 0, 0, 1, 1, 1, 0},
+    {0, 0, 0, 1, 1, 1, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0},
+    {0, 1, 1, 1, 1, 1, 1, 0},
+    {0, 1, 1, 1, 1, 1, 1, 0}};
+
+int N3[8][8] = {
+    {0, 0, 1, 1, 1, 1, 0, 0},
+    {0, 1, 1, 1, 1, 1, 1, 0},
+    {0, 0, 0, 0, 0, 1, 1, 0},
+    {0, 0, 1, 1, 1, 1, 0, 0},
+    {0, 0, 1, 1, 1, 1, 0, 0},
+    {0, 0, 0, 0, 0, 1, 1, 0},
+    {0, 1, 1, 1, 1, 1, 1, 0},
+    {0, 0, 1, 1, 1, 1, 0, 0}};
+
+int display[8][8] = {{0}};
+
+int memory[8][8][3] = {
+    {{0, 0, 0}, {0, 1, 0}, {0, 2, 0}, {0, 3, 0}, {0, 4, 0}, {0, 5, 0}, {0, 6, 0}, {0, 7, 0}},
+    {{1, 0, 0}, {1, 1, 0}, {1, 2, 0}, {1, 3, 0}, {1, 4, 0}, {1, 5, 0}, {1, 6, 0}, {1, 7, 0}},
+    {{2, 0, 0}, {2, 1, 0}, {2, 2, 0}, {2, 3, 0}, {2, 4, 0}, {2, 5, 0}, {2, 6, 0}, {2, 7, 0}},
+    {{3, 0, 0}, {3, 1, 0}, {3, 2, 0}, {3, 3, 0}, {3, 4, 0}, {3, 5, 0}, {3, 6, 0}, {3, 7, 0}},
+    {{4, 0, 0}, {4, 1, 0}, {4, 2, 0}, {4, 3, 0}, {4, 4, 1}, {4, 5, 0}, {4, 6, 0}, {4, 7, 0}},
+    {{5, 0, 0}, {5, 1, 0}, {5, 2, 0}, {5, 3, 0}, {5, 4, 2}, {5, 5, 0}, {5, 6, 0}, {5, 7, 0}},
+    {{6, 0, 0}, {6, 1, 0}, {6, 2, 0}, {6, 3, 0}, {6, 4, 3}, {6, 5, 0}, {6, 6, 0}, {6, 7, 0}},
+    {{7, 0, 0}, {7, 1, 0}, {7, 2, 0}, {7, 3, 0}, {7, 4, 0}, {7, 5, 0}, {7, 6, 0}, {7, 7, 0}}};
+
+char direction = 'u';
+bool end = false;
+bool win = false;
+
+void displaywithtime(int Matrix[][8], unsigned long time = 1000)
+{
+    unsigned long timeupdate = millis();
+    while (millis() - timeupdate < time)
+    {
+        LM.Symbol(Matrix);
+    }
+}
+
+void checkdirection()
+{
+    static unsigned long lastDebounceTime = 0;
+    unsigned long debounceDelay = 50; // 50 ms debounce delay
+    char JYSTCK = direction;          // Default to current direction
+
+    if (millis() - lastDebounceTime > debounceDelay)
+    {
+        if (analogRead(A7) > 768)
+        {
+            JYSTCK = 'l';
+        }
+        else if (analogRead(A7) < 256)
+        {
+            JYSTCK = 'r';
+        }
+        else if (analogRead(A6) > 768)
+        {
+            JYSTCK = 'd';
+        }
+        else if (analogRead(A6) < 256)
+        {
+            JYSTCK = 'u';
+        }
+
+        char oppdirection = 'u'; // Initialize oppdirection with a default value
+
+        switch (direction)
+        {
+        case 'u':
+            oppdirection = 'd';
+            break;
+        case 'd':
+            oppdirection = 'u';
+            break;
+        case 'l':
+            oppdirection = 'r';
+            break;
+        case 'r':
+            oppdirection = 'l';
+            break;
+        default:
+            oppdirection = 'u'; // Default case to ensure oppdirection is always initialized
+            break;
+        }
+
+        if (JYSTCK != oppdirection)
+        {
+            direction = JYSTCK;
+            lastDebounceTime = millis(); // Update debounce time
+        }
+    }
+}
+
+void generateFood()
+{
+    if (!foodExists)
+    {
+        for (int i = 0; i < 64; i++)
+        {
+            Mayfood[i][0] = 0;
+            Mayfood[i][1] = 0;
+        }
+
+        int count = 0;
+
+        // Iterate through the board to find empty spots
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (memory[i][j][2] == 0)
+                { // Check for empty spot
+                    Mayfood[count][0] = i;
+                    Mayfood[count][1] = j;
+                    count++;
+                }
+            }
+        }
+        // If there are empty spots, place food
+        if (count > 0)
+        {
+            int randIndex = random(0, count);
+            memory[Mayfood[randIndex][0]][Mayfood[randIndex][1]][2] = -1;
+            foodExists = true;
+        }
+        else
+        {
+            end = true;
+            win = true;
+            return;
+        }
+    }
+}
+
+void refreshMem()
+{
+    int prelength = length;
+    // Update head position based on direction
+    switch (direction)
+    {
+    case 'u':
+        if (head[0] - 1 < 0)
+        {
+            end = true;
+            return;
+        }
+        else
+        {
+            head[0] = head[0] - 1;
+        }
+        break;
+    case 'd':
+        if (head[0] + 1 > 7)
+        {
+            end = true;
+            return;
+        }
+        else
+        {
+            head[0] = head[0] + 1;
+        }
+        break;
+    case 'l':
+        if (head[1] - 1 < 0)
+        {
+            end = true;
+            return;
+        }
+        else
+        {
+            head[1] = head[1] - 1;
+        }
+        break;
+    case 'r':
+        if (head[1] + 1 > 7)
+        {
+            end = true;
+            return;
+        }
+        else
+        {
+            head[1] = head[1] + 1;
+        }
+        break;
+    }
+
+    // Check if the snake hits itself
+    for (int i = 0; i < length - 1; i++)
+    {
+        if (body[i][0] == head[0] && body[i][1] == head[1])
+        {
+            end = true;
+            return;
+        }
+    }
+
+    // Check if the snake eats the food
+    if (memory[head[0]][head[1]][2] == -1)
+    {
+        prelength++;
+        foodExists = false;
+    }
+
+    for (int i = length - 1; i > 0; i--)
+    {
+        body[i][0] = body[i - 1][0];
+        body[i][1] = body[i - 1][1];
+    }
+    body[0][0] = head[0];
+    body[0][1] = head[1];
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (memory[i][j][2] > 0)
+            {
+                memory[i][j][2] = 0;
+            }
+        }
+    }
+
+    memory[head[0]][head[1]][2] = 1;
+
+    for (int i = 0; i < length - 1; i++)
+    {
+        memory[body[i + 1][0]][body[i + 1][1]][2] = i + 2;
+    }
+
+    length = prelength;
+};
+
+void MemtoDisplay()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (memory[i][j][2] != 0)
+            {
+                display[i][j] = 1;
+            }
+            else
+            {
+                display[i][j] = 0;
+            }
+        }
+    }
+};
+
+void ending()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        LM.Symbol(display);
+        delay(100);
+    }
+
+    for (int c = length; c > 0; c--)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (memory[i][j][2] == c)
+                {
+                    memory[i][j][2] = 0;
+                    MemtoDisplay();
+                    displaywithtime(display, 100);
+                }
+            }
+        }
+    }
+    if (!win)
+    {
+        displaywithtime(End, 10000);
+    }
+    else
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            displaywithtime(Win1, 500);
+        }
+    }
+    exit(0);
+};
+
+void setup()
+{
+    displaywithtime(N3); // Countdown
+    displaywithtime(N2);
+    displaywithtime(N1);
+
+    randomSeed(analogRead(A4));
+
+    MemtoDisplay();
+
+    for (int i = 0; i < 10; i++)
+    {
+        LM.Symbol(display);
+        delay(200);
+    }
+}
+
+void loop()
+{
+    currentMillis = millis();
+
+    if (currentMillis - previousMillis >= interval)
+    {
+        previousMillis = currentMillis;
+        if (!end)
+        {
+            // generateFood();
             checkdirection();
             refreshMem();
             MemtoDisplay();
