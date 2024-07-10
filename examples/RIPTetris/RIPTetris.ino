2a5240aa103b76a4cd13bfe90d7f8c892c6cb2bc
Thedappolice <155630521+Thedappolice@users.noreply.github.com>
2024-07-10 22:13:03 +0800

    "Refactored Tetris game code: changed shape representation, added shape definitions, and modified game logic."

--- examples/RIPTetris/RIPTetris.ino
+++ examples/RIPTetris/RIPTetris.ino
@@ -5,23 +5,36 @@
 #define down 4
 
 int memory[16][8] = {{0}};
-int center[15][7] = {{0}};
 int topLM[8][8];
 int botLM[8][8];
 
-char shapes[7] = {'L', 'J', 'S', 'Z', 'T', 'O', 'I'};
+int (*shapes[7])[4][2] = {&L, &J, &S, &Z, &T, &O, &I};
+
+int J[4][2] = {{3, 15}, {1, 0}, {-1, 0}, {1, 1}};
+int L[4][2] = {{3, 15}, {1, 0}, {-1, 0}, {1, -1}};
+int S[4][2] = {{3, 15}, {1, 0}, {-1, 0}, {-1, -1}};
+int Z[4][2] = {{3, 15}, {-1, 0}, {-1, 0}, {1, -1}};
+int T[4][2] = {{3, 15}, {1, 0}, {-1, 0}, {0, -1}};
+int O[4][2] = {{3, 15}, {-1, 0}, {1, 0}, {-1, -1}};
+int I[4][2] = {{3, 15}, {0, 1}, {0, -1}, {0, -2}};
 
 char command;
 
 bool gotShape = false;
-char shape[4][2] = {{0}};
+int currentShape[4][2] = {{0}};
 int orientation = 1;
 
 void genShape()
 {
-    if (gotShape)
+
+    if (!gotShape)
     {
-        shape = shapes[random(0, 6)];
+        int(*selectedShape)[4][2] = shapes[random(0, 7)];
+        for (int i = 0; i < 4; i++)
+        {
+            currentShape[i][0] = (*selectedShape)[i][0];
+            currentShape[i][1] = (*selectedShape)[i][1];
+        }
     }
 };
 
@@ -29,11 +42,9 @@ void checkBound(char command)
 {
     if (command == 'u')
     {
-
     }
     else if (command == 'r')
     {
-        
     }
 };
 
@@ -64,6 +75,7 @@ void checkInput()
 
 void setup()
 {
+    randomSeed(analogRead(A4));
 }
 
 void loop()
