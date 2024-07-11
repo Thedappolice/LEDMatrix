ef78268321d05c66a3ef408cfb091cfe3ae9f55f
Thedappolice <155630521+Thedappolice@users.noreply.github.com>
2024-07-11 08:12:05 +0800

    Update RIPTetris.ino

--- examples/RIPTetris/RIPTetris.ino
+++ examples/RIPTetris/RIPTetris.ino
@@ -1,4 +1,3 @@
-
 #define rotate 1
 #define left 2
 #define right 3
@@ -23,77 +22,71 @@ char command;
 bool gotShape = false;
 int currentShape[4][2] = {{0}};
 
-void genShape()
-{
-    if (!gotShape)
-    {
-        int(*selectedShape)[4][2] = shapes[random(0, 7)];
-        for (int i = 0; i < 4; i++)
-        {
+void genShape() {
+    if (!gotShape) {
+        int (*selectedShape)[4][2] = shapes[random(0, 7)];
+        for (int i = 0; i < 4; i++) {
             currentShape[i][0] = (*selectedShape)[i][0];
             currentShape[i][1] = (*selectedShape)[i][1];
         }
+        gotShape = true;
     }
-};
+}
 
-void checkBound(char command)
-{
-    if (command == 'u')
-    {
+void checkAndAlter() {
+    if (command == 'o') {
+        int rmb;
+        for (int i = 1; i < 4; i++) {
+            rmb = currentShape[i][0];
+            currentShape[i][0] = currentShape[i][1] * -1;
+            currentShape[i][1] = rmb * -1;
+        }
+    } else if (command == 'l') {
+        currentShape[0][0]--;
+    } else if (command == 'r') {
+        currentShape[0][0]++;
+    } else if (command == 'd') {
+        currentShape[0][1]--;
     }
-    else if (command == 'r')
-    {
-    }
-};
+    command = '\0';
+}
 
-void checkInput()
-{
-    bool leftstate = digitalRead(left) == HIGH;
-    bool rightstate = digitalRead(right) == HIGH;
-    bool downstate = digitalRead(down) == HIGH;
-    bool rotatestate = digitalRead(rotate) == HIGH;
+void checkInput() {
+    bool leftState = digitalRead(left) == HIGH;
+    bool rightState = digitalRead(right) == HIGH;
+    bool downState = digitalRead(down) == HIGH;
+    bool rotateState = digitalRead(rotate) == HIGH;
 
-    if (rotatestate)
-    {
-        command = 'r';
-    }
-    else if (downstate)
-    {
+    if (rotateState) {
+        command = 'o';
+    } else if (downState) {
         command = 'd';
-    }
-    else if (leftstate)
-    {
+    } else if (leftState) {
         command = 'l';
-    }
-    else if (rightstate)
-    {
+    } else if (rightState) {
         command = 'r';
     }
-};
+}
 
-MemtoDisplay()
-{
-    for (int i = 0; i < 16; i++)
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            if (i < 7)
-            {
-                topLM[i][j] = memory[2 * i][2 * j];
-            }
-            else
-            {
-                botLM[i][j] = memory[2 * i][2 * j];
+void MemtoDisplay() {
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (i < 8) {
+                topLM[i][j] = memory[i][j];
+            } else {
+                botLM[i - 8][j] = memory[i][j];
             }
         }
     }
-};
+}
 
-void setup()
-{
+void setup() {
     randomSeed(analogRead(A4));
 }
 
-void loop()
-{
+void loop() {
+    checkInput();
+    checkAndAlter();
+    genShape();
+    MemtoDisplay();
 }
