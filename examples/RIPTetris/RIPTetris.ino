fdb108398345cdb82930437238164133a8380513
Thedappolice <155630521+Thedappolice@users.noreply.github.com>
2024-07-11 11:16:50 +0800

    Update RIPTetris.ino

--- examples/RIPTetris/RIPTetris.ino
+++ examples/RIPTetris/RIPTetris.ino
@@ -6,6 +6,7 @@
 int memory[16][8] = {{0}};
 int topLM[8][8];
 int botLM[8][8];
+int height = 0;
 
 int (*shapes[7])[4][2] = {&L, &J, &S, &Z, &T, &O, &I};
 
@@ -18,7 +19,6 @@ int O[4][2] = {{3, 15}, {-1, 0}, {1, 0}, {-1, -1}};
 int I[4][2] = {{3, 15}, {0, 1}, {0, -1}, {0, -2}};
 
 char command = '\0';
-
 bool gotShape = false;
 int currentShape[4][2] = {{0}};
 
@@ -31,24 +31,6 @@ void genShape() {
         }
         gotShape = true;
     }
-}
-
-void checkAndAlter() {
-    if (command == 'o') {
-        int rmb;
-        for (int i = 1; i < 4; i++) {
-            rmb = currentShape[i][0];
-            currentShape[i][0] = currentShape[i][1] * -1;
-            currentShape[i][1] = rmb * -1;
-        }
-    } else if (command == 'l') {
-        currentShape[0][0]--;
-    } else if (command == 'r') {
-        currentShape[0][0]++;
-    } else if (command == 'd') {
-        currentShape[0][1]--;
-    }
-    command = '\0';
 }
 
 void checkInput() {
@@ -65,7 +47,53 @@ void checkInput() {
         command = 'l';
     } else if (rightState) {
         command = 'r';
-    }else {command = '\0'
+    } else {
+        command = '\0';
+    }
+}
+
+void checkAndAlter() {
+    if (command == 'o') {
+        for (int i = 1; i < 4; i++) {
+            int rmb = currentShape[i][0];
+            currentShape[i][0] = currentShape[i][1] * -1;
+            currentShape[i][1] = rmb * -1;
+        }
+    } else if (command == 'l') {
+        currentShape[0][0]--;
+    } else if (command == 'r') {
+        currentShape[0][0]++;
+    } else if (command == 'd') {
+        currentShape[0][1]--;
+    }
+    command = '\0';
+}
+
+void getHighest() {
+    int count = 0;
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (memory[i][j] == 0) {
+                count++;
+            }
+            if (count == 8) {
+                height = i - 1;
+                break;
+            }
+        }
+    }
+}
+
+void scanGrid() {
+    int count = 0;
+    bool row[15] = {0};
+    int c = 0;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (memory[i][j] == 1) {
+                // Placeholder for functionality
+            }
+        }
     }
 }
 
